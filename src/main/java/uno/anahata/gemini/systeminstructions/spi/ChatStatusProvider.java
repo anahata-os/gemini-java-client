8dfbe31e2fef3d13f122ce444ddd8d59ea3d10ae
priyanahata <p@anahata.uno>
2025-11-04 11:48:07 +0100

    attachFrames if liveWOrkspace is enabled

--- src/main/java/uno/anahata/gemini/systeminstructions/spi/ChatStatusProvider.java
+++ src/main/java/uno/anahata/gemini/systeminstructions/spi/ChatStatusProvider.java
@@ -31,7 +31,9 @@ public class ChatStatusProvider extends SystemInstructionProvider {
         chatStatusBlock.append("- ContextManager: ").append(chat.getContextManager()).append("\n");
         chatStatusBlock.append("- FunctionManager: ").append(chat.getFunctionManager()).append("\n");
         chatStatusBlock.append("- Session Start time: ").append(chat.getStartTime()).append("\n");
+        chatStatusBlock.append("- Live Workspace (attaches screen captures on every call) Enabled: ").append(chat.isLiveWorkspaceEnabled()).append("\n");
         chatStatusBlock.append("- Local Functions Enabled: ").append(chat.isFunctionsEnabled()).append("\n");
+        chatStatusBlock.append("- Server Side Tools (like google search) Enabled: ").append(!chat.isFunctionsEnabled()).append("\n");
         if (chat.getLatency() > 0) {
             chatStatusBlock.append("- Latency (last successfull user/model round trip): ").append(chat.getLatency()).append(" ms.\n");
         }
