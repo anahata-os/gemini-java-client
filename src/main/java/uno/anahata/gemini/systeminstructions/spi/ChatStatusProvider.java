1af2f735b76947fdb0cb44d3630f69da8f97235e
priyanahata <p@anahata.uno>
2025-11-06 14:57:41 +0100

    last api error message

--- src/main/java/uno/anahata/gemini/systeminstructions/spi/ChatStatusProvider.java
+++ src/main/java/uno/anahata/gemini/systeminstructions/spi/ChatStatusProvider.java
@@ -31,14 +31,14 @@ public class ChatStatusProvider extends SystemInstructionProvider {
         chatStatusBlock.append("- ContextManager: ").append(chat.getContextManager()).append("\n");
         chatStatusBlock.append("- FunctionManager: ").append(chat.getFunctionManager()).append("\n");
         chatStatusBlock.append("- Session Start time: ").append(chat.getStartTime()).append("\n");
-        chatStatusBlock.append("- Live Workspace (attaches screen captures on every call) Enabled: ").append(chat.isLiveWorkspaceEnabled()).append("\n");
+        chatStatusBlock.append("- Live Workspace (auto attaches screen captures on every call) Enabled: ").append(chat.isLiveWorkspaceEnabled()).append("\n");
         chatStatusBlock.append("- Local Functions Enabled: ").append(chat.isFunctionsEnabled()).append("\n");
         chatStatusBlock.append("- Server Side Tools (like google search) Enabled: ").append(!chat.isFunctionsEnabled()).append("\n");
         if (chat.getLatency() > 0) {
             chatStatusBlock.append("- Latency (last successfull user/model round trip): ").append(chat.getLatency()).append(" ms.\n");
         }
         if (chat.getLastApiError() != null) {
-            chatStatusBlock.append("- Last API Error: \n").append(new Date()).append("\n").append(chat.getLastApiError()).append("\n");
+            chatStatusBlock.append("- Last API Error: \n").append(chat.getLastApiError()).append("\n");
         }
         
         return Collections.singletonList(Part.fromText(chatStatusBlock.toString()));
