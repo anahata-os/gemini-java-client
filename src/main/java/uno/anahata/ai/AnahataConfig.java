27bd71f0f00852dbbe84a3a5438dd6a9b1244231
priyanahata <p@anahata.uno>
2025-12-24 12:21:51 +0100

    licensing

--- src/main/java/uno/anahata/ai/AnahataConfig.java
+++ src/main/java/uno/anahata/ai/AnahataConfig.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+/* Licensed under the Apache License, Version 2.0 */
 package uno.anahata.ai;
 
 import java.io.File;
